c0ac270db5dcc2c3af4262bb36263a0a40a1bb51
Muhammad Osama <72553062+osama-shakil@users.noreply.github.com>
2025-01-13 00:13:59 +0500

    HeadLine   fix done

--- src/components/landing/HeadLine.jsx
+++ src/components/landing/HeadLine.jsx
@@ -1,31 +1,24 @@
-'use client';
-import React from 'react';
+
+"use client";
+import React from "react";
 
 const HeadLine = () => {
-	
-	return (
-		<>
-			<div
-				
-				className='border-y border-black relative overflow-hidden whitespace-nowrap bg-white'
-			>
-				<div className='inline-block animate-[marquee_25s_linear_infinite] py-4'>
-					<span className='inline-block'>
-						VIEW COLLECTIONS - CREATE WALLET - VIEW COLLECTIONS -
-						CREATE WALLET - VIEW COLLECTIONS - CREATE WALLET - VIEW
-						COLLECTIONS - CREATE WALLET -
-					</span>
-				</div>
-				{/* <div className='absolute top-0 inline-block animate-[marquee2_25s_linear_infinite] py-4'>
-					<span className='inline-block'>
-						VIEW COLLECTIONS - CREATE WALLET - VIEW COLLECTIONS -
-						CREATE WALLET - VIEW COLLECTIONS - CREATE WALLET - VIEW
-						COLLECTIONS - CREATE WALLET -
-					</span>
-				</div> */}
-			</div>
-		</>
-	);
+  const text = " VIEW COLLECTIONS - CREATE WALLET - ";
+  // Duplicate the text multiple times to ensure smooth looping
+  const repeatedText = `${text} ${text} ${text} ${text} ${text} ${text}`;
+
+  return (
+    <div className="relative w-full overflow-hidden border-y border-black  py-4">
+      <div className="flex w-full">
+        <div className="marquee-scroll flex whitespace-nowrap min-w-full">
+          <span className="mx-4  text-md">{repeatedText}</span>
+        </div>
+        <div className="marquee-scroll flex whitespace-nowrap min-w-full">
+          <span className="mx-4 text-md">{repeatedText}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeadLine;
